e9482bd7729ab1b2fa2d4d153708beb2a6dc0424
TE109 <1991tmce@gmail.com>
2025-03-06 13:31:29 -0500

    Adding description

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Apple And Web Weather App ",
     subtitle: "Weather App ",
     description:
-      "This is a placeholder showing how to add an element to projects",
+      "This application interfaces with a weather API to retrieve real-time weather data. It then presents this data to users in a clean, formatted interface for easy access to weather information.",
     image: "./gif-files/Weather_app.gif",
     link: "https://github.com/TE109/Apple-and-Web-weather-app.git",
   }, 
@@ -12,7 +12,7 @@ export const projects = [
     title: "Socket.io Chat app ",
     subtitle: "1 to 1 and 1 to group chat messaging app",
     description:
-      "This is a placeholder showing how to add an element to projects",
+      "This chat application allows multiple users to create accounts, log in, and engage in conversations users can send messages to predefined groups leveraging real-time communication via Socket.io.",
     image: "./gif-files/1_1 Chat.gif",
     link: "https://github.com/TE109/1to1_Socketio_chat_App.git",
   },
@@ -20,7 +20,7 @@ export const projects = [
     title: "Movie Review Analysis",
     subtitle: "IMDB review AI analysis",
     description:
-      "This is a placeholder showing how to add an element to projects",
+      "This AI model is trained to classify movie reviews as either positive or negative. Using the IMDB dataset from Keras.",
     image: "./gif-files/AI_Gif.gif",
     link: "https://github.com/TE109/Movie-Review-Analysis.git",
   },
@@ -28,7 +28,7 @@ export const projects = [
     title: "User and Employee graphql implementation ",
     subtitle: "GraphQL API",
     description:
-      "This is a placeholder showing how to add an element to projects",
+      "This custom GraphQL API enables the creation of authorized users, allowing them to securely log into their accounts. Additionally, it supports CRUD operations on employee data, including the creation, editing, and deletion of employee records stored in a MongoDB database.",
     image: "./gif-files/GraphQL_Project_Gif.gif",
     link: "https://github.com/TE109/User-and-Employee-graphql-implementation.git",
   },
